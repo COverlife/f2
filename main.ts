391030b2e20f686215ab460177a261ba084ea089
COverlife <87011901+COverlife@users.noreply.github.com>
2021-07-07 22:37:30 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,79 @@
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.greenOuterSouthWest, function (sprite, location) {
+    mySprite.setPosition(230, 225)
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.greenInnerSouthEast, function (sprite, location) {
+    mySprite.setPosition(230, 225)
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.greenOuterEast0, function (sprite, location) {
+    mySprite.setPosition(230, 225)
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.greenOuterNorthWest, function (sprite, location) {
+    mySprite.setPosition(230, 225)
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.greenInnerNorthEast, function (sprite, location) {
+    mySprite.setPosition(230, 225)
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.greenOuterWest0, function (sprite, location) {
+    mySprite.setPosition(230, 225)
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.greenOuterNorthEast, function (sprite, location) {
+    mySprite.setPosition(230, 225)
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.greenOuterSouth2, function (sprite, location) {
+    mySprite.setPosition(230, 225)
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.greenOuterNorth2, function (sprite, location) {
+    mySprite.setPosition(230, 225)
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.greenOuterEast2, function (sprite, location) {
+    mySprite.setPosition(230, 225)
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.greenOuterEast1, function (sprite, location) {
+    mySprite.setPosition(230, 225)
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.greenOuterSouth0, function (sprite, location) {
+    mySprite.setPosition(230, 225)
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.greenOuterSouth1, function (sprite, location) {
+    mySprite.setPosition(230, 225)
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.greenOuterSouthEast, function (sprite, location) {
+    mySprite.setPosition(230, 225)
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.greenOuterWest2, function (sprite, location) {
+    mySprite.setPosition(230, 225)
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.greenOuterNorth1, function (sprite, location) {
+    mySprite.setPosition(230, 225)
+})
+let mySprite: Sprite = null
+mySprite = sprites.create(img`
+    ........................
+    ........................
+    ........................
+    ........................
+    ..........ffff..........
+    ........ff1111ff........
+    .......fb111111bf.......
+    .......f11111111f.......
+    ......fd11111111df......
+    ......fd11111111df......
+    ......fddd1111dddf......
+    ......fbd1fddf1dbf......
+    ......fcd3f11f3dcf......
+    .......fb111111bf.......
+    ......fffcdb1bdffff.....
+    ....fc111cbfbfc111cf....
+    ....f1b1b1ffff1b1b1f....
+    ....fbfbffffffbfbfbf....
+    .........ffffff.........
+    ...........fff..........
+    ........................
+    ........................
+    ........................
+    ........................
+    `, SpriteKind.Player)
+mySprite.setPosition(230, 225)
+controller.moveSprite(mySprite)
+tiles.setTilemap(tilemap`level1`)
+scene.cameraFollowSprite(mySprite)
---
+++ tilemap.g.ts
@@ -0,0 +1,39 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level1":
+            case "level1":return tiles.createTilemap(hex`1000100003070f070707070707070707070f1009060e0e0e0e0e0e0e0e0e0e0e0e0e0e09060e0e0a110a0a0a0a0a0a110a0a0a09060e0e0e0e0e0e0e0e0e0e0e0e0e0e09130e0e0e0e0e0e0e0e0e0e0e0e0e0e12060e0e0e0e0e0e0e0e0e0e0e0e0e0e09060e0e0e0e0e0e0e0e0e0e0e0e0e0e090607020a0a110a0a0a0a0a0a0a0a0e09060c040e0e0e0e0e0e0e0e0e0e0e0e09060d090e0e0e0e0e0e0e0e0e0e0e0e09060d120e0e0e0e0e0e0e0e0e0e0e0e12060d040e0e0e0e0e0e0e0e0e0e0e0e09060b040e070f070707070707070707090608050e0e0e0e0e0e0e0e0e0e0e0e09060e0e0e0e0e0e0e0e0e0e0e0e0e0e0901080808080808080808080808081105`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16,sprites.dungeon.greenOuterSouthEast,sprites.dungeon.greenOuterNorthEast,sprites.dungeon.greenOuterNorthWest,sprites.dungeon.greenOuterEast1,sprites.dungeon.greenOuterSouthWest,sprites.dungeon.greenOuterWest0,sprites.dungeon.greenOuterNorth1,sprites.dungeon.greenOuterSouth0,sprites.dungeon.greenOuterEast0,sprites.dungeon.greenOuterSouth1,sprites.dungeon.greenInnerSouthEast,sprites.dungeon.greenInnerNorthEast,sprites.dungeon.darkGroundSouthEast1,sprites.dungeon.floorLight1,sprites.dungeon.greenOuterNorth2,sprites.dungeon.stairLarge,sprites.dungeon.greenOuterSouth2,sprites.dungeon.greenOuterEast2,sprites.dungeon.greenOuterWest2], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
